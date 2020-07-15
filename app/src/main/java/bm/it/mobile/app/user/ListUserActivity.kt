ae3ecae1c265c45a74eeeff444d1180dea5c312f
Thales Marega <thales.bm92@gmail.com>
2020-07-15 09:20:48 -0300

    first commit

---
+++ app/src/main/java/bm/it/mobile/app/user/ListUserActivity.kt
@@ -0,0 +1,14 @@
+package bm.it.mobile.app.user
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class ListUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Toast.makeText(this, "ListUserActivity", Toast.LENGTH_LONG).show()
+    }
+}
